28cd908befcfa5cc2f549572f57b148d55df399f
tartur88@gmail.com <tartur88@gmail.com@d05591d2-05f3-1469-67f1-8d95665da4b1>
2013-01-21 09:21:40 +0000

    added junit lib to buildpath

--- ServerSimpleIM/src/serversimpleim/Main.java
+++ ServerSimpleIM/src/serversimpleim/Main.java
@@ -4,7 +4,7 @@ package serversimpleim;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		new UdpServer().run();
+		new UdpServer().run(); /* by running this we start the server on default port - 4445 */
 
 	}
 
